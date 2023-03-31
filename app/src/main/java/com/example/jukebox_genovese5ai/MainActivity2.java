0d9dd6a6c65ed311f53f4fb7ea991d3443b9e817
Giuseppe Genovese <ggenovese141@gmail.com>
2023-03-31 16:11:17 +0200

    agg commit

---
+++ app/src/main/java/com/example/jukebox_genovese5ai/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.jukebox_genovese5ai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
