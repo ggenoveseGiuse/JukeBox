a0538144e0ef063fdfca34435fe37cb14dfc0a70
Giuseppe Genovese <ggenovese141@gmail.com>
2023-04-13 08:41:35 +0200

    update 1304

--- app/src/main/java/com/example/jukebox_genovese5ai/MainActivity.java
+++ app/src/main/java/com/example/jukebox_genovese5ai/MainActivity.java
@@ -2,35 +2,71 @@ package com.example.jukebox_genovese5ai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    TextView text1;
+    Button cambiaActivity;
+    int n; //numero random
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         EditText txtNome = findViewById(R.id.txtNome);
         // file R.java è un file dove finiscono tutti i puntatori
         // relativi agli oggetti istanziati
 
         Button btnInvia = findViewById(R.id.btnInvia);
-        
-        // associare un'altro oggeetto all'ascoltatore 
+        Button btnRandom = findViewById(R.id.btnRandom);
+        Button btnAvvia = findViewById(R.id.btnAvvia);
+
+        // associare un'altro oggeetto all'ascoltatore
         // l'ascoltatore è una classe anonima
         btnInvia.setOnClickListener(new View.OnClickListener() {    // classe astratta
             // istanziare un'interfaccia (OnClickListener)
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                intent.putExtra("Numero", 10);
+                startActivity(intent);
             }
-        });
-        
+        }
+        );
 
     }
-}
+
+
+    public void generaNumero(View view){
+
+        Random rand = new Random();
+        int n = rand.nextInt(10);
+        text1.setText(Integer.toString(n+1));
+        cambiaActivity.setVisibility(View.VISIBLE);
+    }
+
+
+    public void cambiaActivity(View view){
+        Intent ActivityMusic = new Intent(getApplicationContext(), MainActivity2.class);
+        ActivityMusic.putExtra("NumeroRandom", n);
+        startActivity(ActivityMusic);
+
+    }
+
+
+    }
--- app/src/main/java/com/example/jukebox_genovese5ai/MainActivity2.java
+++ app/src/main/java/com/example/jukebox_genovese5ai/MainActivity2.java
@@ -2,13 +2,43 @@ package com.example.jukebox_genovese5ai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
 
 public class MainActivity2 extends AppCompatActivity {
+
+    final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+
+        Bundle bundle = intent.getExtras();
+        int numero = bundle.getInt("Numero");
+
+        Log.d(TAG, "Numero passato: " + numero);
+
+        String titolo = "";
+        if(numero == 10){
+            titolo = "Cenere";
+        }
+        TextView txttitolo = findViewById(R.id.txtTitolo);
+        txttitolo.setText(titolo);
+
+        ImageView image = new ImageView(MainActivity2.this);
+
+
+        ImageView immagine = findViewById(R.id.idImmagine);
+        immagine.setImageResource(R.drawable.cover);
+        //immagine.setImageDrawable(R.id.cover);
+        //ImageView image = new ImageView();
     }
 }
